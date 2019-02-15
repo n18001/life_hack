aed208d66bc2ef56cd456bbc1cc4b7deaf30e7e4
n18001 <n18001@std.it-college.ac.jp>
2019-02-15 10:22:08 +0900

    0215

--- gitadd_push.py
+++ gitadd_push.py
@@ -2,25 +2,35 @@
 
 
 # gitのadd commit pushを一気にやるよ
+# pyautoguiを入れてね
+#
 # 初期化とかは自分でやってほしいよ
-# commit は日付でするよ
+# commitは日付でするよ
 import pyautogui as gui
+from datetime import datetime
 
 
-
+# 新しいのを作るよ
+gui.hotkey('ctrl', 'shift', 'n')
+gui.hotkey('winleft', 'left')
+gui.click(240, 370, duration=0.25)
 
 
 # git add
-gui.typewrite('git add -u', 0.1)
+gui.typewrite('git add .', 0.05)
 gui.press('enter')
 
 
 # git commit
-comment = input('comment :')
-gui.typewrite("git commit -m '{}'".format(comment), 0.1)
+today = datetime.now().strftime('%m%d')
+gui.typewrite("git commit -m '{}'".format(today), 0.05)
 gui.press('enter')
 
 
 # git push
-gui.typewrite('git push origin master', 0.1)
+gui.typewrite('git push origin master', 0.05)
 gui.press('enter')
+
+
+# 気持ち悪い動作だよ
+# 綺麗に書くにはどうしたら良いんだよ
