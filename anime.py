486d09b0c159a2ee889730099b90f56e0e3c9e1f
n18001 <n18001@std.it-college.ac.jp>
2019-02-28 12:41:07 +0900

    0228

---
+++ anime.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+
+# アニメを取ってくるだけ
+import requests, json
+from datetime import datetime
+
+
+# URL
+def getAnimeURL(year, cours):
+    URL = 'http://api.moemoe.tokyo/anime/v1/master/{}/{}'.format(year, cours)
+    return URL
+
+
+# Jsonを読む
+def readJson(url):
+    r = requests.get(url)
+    data = json.loads(r.text)
+    return data
+
+
+# アニメの名前を取得
+def getAnimeName(x):
+    anime_list = []
+    for animest in range(len(x)):
+        info = '・{}: {}'.format(x[animest]['title'], x[animest]['public_url'])
+        anime_list.append(info)
+    return anime_list
+
+
+# アニメの名前を出力
+def showAnimeName(x):
+    for name in x:
+        desplay(name)
+
+
+# ディスプレイ
+def desplay(d):
+    print(d)
+
+
+# 実行
+if __name__ == '__main__':
+    while True:
+        try:
+            year = int(input('年: '))
+            cours = int(input('期(1~4): '))
+            if not (2014 <= year <= int(datetime.now().strftime('%Y'))) or not (1 <= cours <= 4):
+                desplay('Error')
+                break
+            else:
+                showAnimeName(getAnimeName(readJson(getAnimeURL(year, cours))))
+                break
+        except ValueError:
+            desplay('ERROR')
+            break
